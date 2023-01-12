05f8d4b078856a3595a15de253605ab3d08a0ec0
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2023-01-12 11:07:57 -0500

    loop

--- Program.fs
+++ Program.fs
@@ -1,5 +1,8 @@
 ﻿//open system
 
+// Alex Schmaus 1/12/2023
+// Playing around with F#
+
 let msg = "Hello World! How are you today?"
 
 if 1 = 1 then
@@ -7,7 +10,17 @@ if 1 = 1 then
 else
     printfn "The heck??"
 
+
+
 let greetFunction message = 
     printfn $"Message is: {message}"
 
 greetFunction "twenty three skidoo"
+
+
+
+
+let names = ["Bob"; "Sally"; "Susan"; "Bill"]
+
+for n in names do
+    printfn "Hello %s" n
